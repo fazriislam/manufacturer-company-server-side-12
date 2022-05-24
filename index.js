43bc22a11b854df8324a4d17c05dc43dac1a24bd
Fazri Isalm <fazriislam.work@gmail.com>
2022-05-24 20:33:44 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@doctors.5ryvs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+
+    }
+
+
+    finally {
+
+    }
+}
+
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('App is running for assignment 12')
+})
+
+app.listen(port, () => {
+    console.log(`App is listening ${port}`)
+})
