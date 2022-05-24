95be295eb6f116f209cbcab4eff918311a978a29
Fazri Isalm <fazriislam.work@gmail.com>
2022-05-24 22:24:54 +0600

    Create product API

--- index.js
+++ index.js
@@ -9,16 +9,27 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json())
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@doctors.5ryvs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@company-owner.zklwg.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 
 async function run() {
     try {
         await client.connect();
+        const productCollection = client.db('manufacturerCompany').collection('products');
+
+
+        // ----------------all GET APT
+        app.get('/product', async (req, res) => {
+            const products = await productCollection.find().toArray();
+            res.send(products);
+        })
 
     }
 
